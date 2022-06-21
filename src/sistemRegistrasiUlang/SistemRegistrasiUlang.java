7dd6d01a8e4e94f0bbb4cfc7c97efb4d13410c66
hans.abdillh05@gmail.com <hans.abdillh05@gmail.com>
2022-06-21 10:08:07 +0800

    Create UI

---
+++ src/sistemRegistrasiUlang/SistemRegistrasiUlang.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemRegistrasiUlang;
+
+// Import all panels
+
+import sistemRegistrasiUlang.UserPanel.MainForm;
+
+// import sistemRegistrasiUlang.UserPanel.*;
+
+/**
+ *
+ * @author hp
+ */
+public class SistemRegistrasiUlang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        sistemRegistrasiUlang.UserPanel.MainForm userPanel = new sistemRegistrasiUlang.UserPanel.MainForm();
+        sistemRegistrasiUlang.Home.MainForm homePanel = new sistemRegistrasiUlang.Home.MainForm();
+        
+        homePanel.setVisible(true);
+    }
+    
+}
